50c44ecd66e30c2bbb7be37047a48e2280145731
Ykarin <mistelinn@gmail.com>
2018-06-12 15:57:04 +0300

    Add BNetProgramId

---
+++ bnet-rpc/src/main/java/com/d3emu/bnet/rpc/BNetProgramId.java
@@ -0,0 +1,8 @@
+package com.d3emu.bnet.rpc;
+
+public class BNetProgramId {
+
+    public static final FourCC DIABLO3 = new FourCC("D3");
+
+    public static final FourCC BNET = new FourCC("BN");
+}
