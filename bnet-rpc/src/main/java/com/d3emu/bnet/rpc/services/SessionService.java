d8e22c5e8d6ed3f1d1d318275a517ec333891008
Ykarin <mistelinn@gmail.com>
2018-06-12 15:55:02 +0300

    Add very base createSession impl

--- bnet-rpc/src/main/java/com/d3emu/bnet/rpc/services/SessionService.java
+++ bnet-rpc/src/main/java/com/d3emu/bnet/rpc/services/SessionService.java
@@ -2,8 +2,12 @@ package com.d3emu.bnet.rpc.services;
 
 import java.util.logging.Logger;
 
-import bnet.protocol.RpcProto.NoData;
+import bnet.protocol.account.v1.AccountTypesProto.*;
+import bnet.protocol.session.v1.SessionListener;
 import bnet.protocol.session.v1.SessionServiceProto.*;
+import bnet.protocol.RpcProto.NoData;
+
+import com.d3emu.bnet.rpc.BNetProgramId;
 
 import com.google.protobuf.RpcCallback;
 
@@ -15,6 +19,24 @@ public final class SessionService extends bnet.protocol.session.v1.SessionServic
 
     public final void createSession(ChannelHandlerContext ctx, CreateSessionRequest request, RpcCallback<CreateSessionResponse> done) {
         logger.info(request.toString());
+
+        String session = "A7B5C8B0593FFEC100000000000BCABD";  // FIXME: properly generate session
+        // TODO: store and expire generated sessions?
+        CreateSessionResponse.Builder builder = CreateSessionResponse.newBuilder();
+        builder.setSessionId(session);
+
+        done.run(builder.build());
+        
+        SessionCreatedNotification.Builder n = SessionCreatedNotification.newBuilder();
+        AccountId.Builder a = AccountId.newBuilder();
+        GameAccountHandle.Builder ga = GameAccountHandle.newBuilder();
+        a.setId(1);  // FIXME: properly set BNet account id
+        ga.setId(1)  // FIXME: properly set D3 account id
+          .setProgram(BNetProgramId.DIABLO3.getValue())
+          .setRegion(1);  // US
+        n.setIdentity(Identity.newBuilder().setAccount(a).setGameAccount(ga));
+        n.setReason(0).setSessionId(session);
+        SessionListener.newStub().onSessionCreated(ctx, n.build());
     }
 
     public final void destroySession(ChannelHandlerContext ctx, DestroySessionRequest request, RpcCallback<NoData> done) {}
